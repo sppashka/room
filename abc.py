3ca671e122af0d26668c0b9d514d052c9913b6e7
ignat99 <ignat99@gmail.com>
2019-03-16 01:04:54 +0300

    First commit

---
+++ abc.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+#-*-coding:utf-8-*-
+
+
+from __future__  import print_function
+
+
+#print("Vvedite chislo - skolko duimov?")
+#duimov = input()
+#santimetrov  = duimov * 2.54
+#print("santimetrov", santimetrov)
+
+#print("Vvedite chislo - skolko kilometrov?")
+#kilometrov = input()
+#metrov  = kilometrov * 1000
+#print("Metrov", metrov)
+
+
+
+print("Vvedite chislo - skolko funtov?")
+funtov = input()
+kilogramm  = funtov * 0.45
+print("kilogramm", kilogramm)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
